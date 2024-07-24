3a9cf3af029cb4dd8fa9374e27a7188e55e97ab7
islamBelabbes <belabbesislam2@gmail.com>
2024-07-24 10:56:38 +0100

    fix react props

--- src/app/_components/top-bar.tsx
+++ src/app/_components/top-bar.tsx
@@ -4,8 +4,8 @@ function TopBar() {
   return (
     <div className="w-full bg-red-700 py-4 text-center font-semibold capitalize tracking-wide text-white">
       <p>
-        please note that these Banners are not responsive for : 1 - i'm lazy. 2
-        - there is no figma version)
+        please note that these Banners are not responsive for : 1 - i&apos;m
+        lazy. 2 - there is no figma version)
       </p>
     </div>
   );
